a0b5779e4fdf3b24b43faf9c59efe6d7a87ff736
shadeTheDev <shadeTheDev@gmail.com>
2024-07-11 16:13:41 -0500

    Set Programs Tab images

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
+import Programs from "./Components/Programs/Programs.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Programs />
     </div>
   );
 };
