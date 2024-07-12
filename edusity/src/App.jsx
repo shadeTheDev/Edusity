f8f24e51f07122bd92f29d6c1def37ac4e63ea70
shadeTheDev <shadeTheDev@gmail.com>
2024-07-11 20:53:25 -0500

    About and Programs finished

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import Programs from "./Components/Programs/Programs.jsx";
+import Title from "./Components/Title/Title.jsx";
+import About from "./Components/About/About.jsx";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Programs />
+      <div className="container">
+        <Title subTitle="Our Program" title="What We Offer" />
+        <Programs />
+        <About />
+      </div>
     </div>
   );
 };
