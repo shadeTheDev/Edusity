89218b65427a6af2c1d6a2df114ad81fcb7eade0
shadeTheDev <shadeTheDev@gmail.com>
2024-07-16 19:13:44 -0500

    Finished Gallery and Testimonials
    FInished the gallery and testimonials page that adds a slider to scroll testimonials

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./Components/Hero/Hero.jsx";
 import Programs from "./Components/Programs/Programs.jsx";
 import Title from "./Components/Title/Title.jsx";
 import About from "./Components/About/About.jsx";
+import Campus from "./Components/Campus/Campus.jsx";
+import Testimonials from "./Components/Testimonials/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -14,6 +16,10 @@ const App = () => {
         <Title subTitle="Our Program" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="Testimonials" title="What Student's Say" />
+        <Testimonials />
       </div>
     </div>
   );
